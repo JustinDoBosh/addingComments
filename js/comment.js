3894bf18344e85d3a8d51b43726aca31d8e8d225
JustinDoBosh <justin.dobosh@spartans.ut.edu>
2014-12-22 19:26:42 -0600

    updates

--- js/comment.js
+++ js/comment.js
@@ -1,7 +1,15 @@
 function commentController($scope) {
             //initalize default values
         
-			$scope.commentList = [{"fullName":"John Doe", "usrComment":"Lorem ipsum dolor sit amet, commodo viverra aliquam dis, sollicitudin varius lacus turpis amet mauris sem. Porta purus molestie ", "count":4}, {"fullName":"Jane Doe", "usrComment":"Lorem ipsum dolor sit amet, commodo viverra aliquam dis, sollicitudin varius lacus Lorem ipsum dolor sit amet, iverra aliquam dis, sollicitudin varius lacus turpis amet.", "count":6}];
+			$scope.commentList = [
+			{"fullName":"John Doe",
+			 "usrComment":"Lorem ipsum dolor sit amet, commodo viverra aliquam dis, sollicitudin varius lacus turpis amet mauris sem. Porta purus molestie ",
+			  "count":4},
+			
+		    {"fullName":"Jane Doe", 
+			"usrComment":"Lorem ipsum dolor sit amet, commodo viverra aliquam dis, sollicitudin varius lacus Lorem ipsum dolor sit amet, iverra aliquam dis, sollicitudin varius lacus turpis amet.", 
+			"count":6}
+			];
 
 			$scope.commentAdd = function(){
 				$scope.commentList.push({fullName:$scope.fullName, usrComment:$scope.usrComment, count:$scope.count});
